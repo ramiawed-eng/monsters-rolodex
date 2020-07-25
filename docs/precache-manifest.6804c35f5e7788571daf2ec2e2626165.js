d1ff2a116a71a4df631770ccc03e94d5aa26c235
ramiawed-eng <ramiawed-eng@gmail.com>
2020-07-25 21:03:59 +0300

    adding files for github pages

---
+++ docs/precache-manifest.6804c35f5e7788571daf2ec2e2626165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d190295c6b271c106a0af29b34952c39",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "36f2f69138ef4813a944",
+    "url": "/monster-rolodex/static/css/main.9b0e2a2b.chunk.css"
+  },
+  {
+    "revision": "5ba5ac096b0d3445fa8f",
+    "url": "/monster-rolodex/static/js/2.a8bb8d99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a8bb8d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f2f69138ef4813a944",
+    "url": "/monster-rolodex/static/js/main.993b7e08.chunk.js"
+  },
+  {
+    "revision": "b70a9c55c535ea67c1fc",
+    "url": "/monster-rolodex/static/js/runtime-main.80f93c1b.js"
+  }
+]);
